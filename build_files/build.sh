261b254bc07d89ee82044a1f62ba6217382a98bd
amarevite <35640607+amarevite@users.noreply.github.com>
2025-10-21 20:56:58 -0700

    + earlyoom

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux
 
 dnf5 config-manager setopt terra.enabled=1
 dnf5 install -y espanso-wayland
@@ -37,7 +37,10 @@ dnf5 -y install intel-compute-runtime
 dnf5 -y install intel-level-zero-gpu-raytracing
 dnf5 -y install embree
 
+dnf5 -y install earlyoom
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable earlyoom.service
 setcap "cap_dac_override+p" "$(which espanso)"
