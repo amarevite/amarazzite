4a6b3e7be57065c1caf00ced68cdab05ee580a35
amarevite <35640607+amarevite@users.noreply.github.com>
2026-06-11 22:50:51 -0700

    move kde-material-you-colors to userspace

--- build_files/build.sh
+++ build_files/build.sh
@@ -26,9 +26,9 @@ dnf5 -y copr enable matinlotfali/KDE-Rounded-Corners
 dnf5 -y install kwin-effect-roundcorners
 dnf5 -y copr disable matinlotfali/KDE-Rounded-Corners
 
-dnf5 -y config-manager addrepo --from-repofile="https://download.opensuse.org/repositories/home:luisbocanegra/Fedora_$OS_VERSION/home:luisbocanegra.repo"
-dnf5 -y install kde-material-you-colors --allowerasing
-dnf5 -y config-manager setopt home_luisbocanegra.enabled=0
+# dnf5 -y config-manager addrepo --from-repofile="https://download.opensuse.org/repositories/home:luisbocanegra/Fedora_$OS_VERSION/home:luisbocanegra.repo"
+# dnf5 -y install kde-material-you-colors --allowerasing
+# dnf5 -y config-manager setopt home_luisbocanegra.enabled=0
 
 dnf5 -y copr enable neilalexander/yggdrasil-go
 dnf5 -y install yggdrasil
